889c27fd89c39b89c6dee7d136d0201e1362e6b3
adoubt <gerya1508@gmail.com>
2026-04-09 18:13:27 +0700

    +content

--- src/misc.py
+++ src/misc.py
@@ -13,9 +13,12 @@ LOG_CHANNEL_LINK = os.getenv('LOG_CHANNEL_LINK')
 LOG_CHANNEL_ID = os.getenv('LOG_CHANNEL_ID')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 CHANNEL_LINK = os.getenv('CHANNEL_LINK')
-START_MESSAGE_PHOTO = FSInputFile("content/start_message.jpg")
-START_MESSAGE_PHOTO2 = FSInputFile("content/start_message2.jpg")
-
+PHOTO = FSInputFile("content/photo1.jpg")
+PHOTO2 = FSInputFile("content/photo2.jpg")
+PHOTO3 = FSInputFile("content/photo3.jpg")
+PHOTO4 = FSInputFile("content/photo4.jpg")
+PHOTO5 = FSInputFile("content/photo5.jpg")
+PHOTO6 = FSInputFile("content/photo6.jpg")
 
 bot_id = BOT_TOKEN.split(":",1)[0]
 bot = Bot(BOT_TOKEN)
